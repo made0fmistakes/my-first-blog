4d014cc69f6ff2fc29730a006044ae5e90cde522
made0fmistakes <omniring@gmail.com>
2020-04-02 22:01:51 +0530

    Modified the blog-urls file by removing that r and ^

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 
 ]
